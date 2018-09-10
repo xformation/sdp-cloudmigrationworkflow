3f193b52591fdafe4f02a39b88d9b96cb4f4fb7c
Anju <sharmanju80@gmail.com>
2018-09-10 23:25:08 +0530

    update controller annotation.

--- src/main/java/com/synectiks/services/migration/controllers/MigrationController.java
+++ src/main/java/com/synectiks/services/migration/controllers/MigrationController.java
@@ -11,11 +11,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.synectiks.commons.constants.IConsts;
@@ -26,7 +26,7 @@ import com.synectiks.services.migration.services.MigrationWorkflowService;
 /**
  * @author Rajesh
  */
-@Controller
+@RestController
 @RequestMapping(path = IApiController.API_PATH
 		+ IApiController.URL_MIGRATION, method = RequestMethod.POST)
 @CrossOrigin
